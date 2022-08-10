e74c0f28af86584201636d11855f653ff3439876
Nahid Kamal <nahidmkamal@gmail.com>
2022-08-10 12:59:12 -0400

    sample python script to run selenium tests

---
+++ starter-test.py
@@ -0,0 +1,28 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.by import By
+import time
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+
+driver.get('https://oviahealthcoachingdashboard--partial.lightning.force.com/lightning/n/Inbox_Messages_List')
+print('Page has loaded with title: ' + driver.title)
+
+username_field = driver.find_element(By.ID, 'username')
+ActionChains(driver).click(on_element = username_field).send_keys('nahid-coach@oviahealth.com.partial').perform()
+time.sleep(1)
+
+password_field = driver.find_element(By.ID, 'password')
+ActionChains(driver).click(on_element = password_field).send_keys('OviaTester123!').perform()
+time.sleep(1)
+
+login_button = driver.find_element(By.ID, 'Login')
+ActionChains(driver).click(on_element = login_button).perform()
+time.sleep(5)
+
+
+
+driver.quit()
+
